e52ca48379cba23c8261aad27cc5eb57084d12c7
Phong Nguyễn <pitireg@gmail.com>
2016-09-13 12:05:52 +0000

    Update: OK_sample_app

--- OK_sample_app/bin/startup.sh
+++ OK_sample_app/bin/startup.sh
@@ -5,6 +5,6 @@ export OKSP_ENV=test_environment_value
 mkdir $OKSP_HOME_PATH/logs
 
 cd $OKSP_HOME_PATH/bin
-./node main.js > $OKSP_HOME_PATH/logs/ok_sample_app.log 2>$OKSP_HOME_PATH/logs/ok_sample_app.err.log  &
+./node ok_sample_app.js > $OKSP_HOME_PATH/logs/ok_sample_app.log 2>$OKSP_HOME_PATH/logs/ok_sample_app.err.log  &
 
 echo '"Sample app" is started!'

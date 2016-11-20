2a557644ff81419a33084605d9f1b015d5cafcdc
Phong Nguyễn <pitireg@gmail.com>
2016-11-20 01:39:12 +0000

    Update OK_HeadQuater Update startup flow

--- OK_HeadQuater/run.sh
+++ OK_HeadQuater/run.sh
@@ -4,6 +4,7 @@ export OK_ZK_PORT='2181'
 export OK_ZK_MAIN_CONF='/danko/conf'
 export OK_ZK_APPNAME='headquater' ## For HeadQuater - Don't change this config
 export OK_LOGFILE='./logs/danko.log'
+export OK_PID_FILE='./pid.txt'
 
 # For debug
 export DEBUG=*

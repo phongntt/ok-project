7adb00fe3dcb52a15344d1391db01ea5b73e592c
Nguyễn Trần Tuấn Phong <nttpbt@gmail.com>
2017-04-24 06:09:22 +0000

    OK_Deployer Update - file_sender - command_runner

--- OK_Deployer/ok_modules/command_runner.js
+++ OK_Deployer/ok_modules/command_runner.js
@@ -1,9 +1,119 @@
+const fs = require('fs');
+const ssh2 = require('ssh2');
+
+
+/**
+* SSH to remote machine and run the @command_string on it.
+* Params:
+* @command_string: command to run on remote machine
+*	@processor: an object to process the result of command.This contain 2 properties
+*	{
+*	  connection: will be set by this fuction. This value is the SSH connection that this function opened.
+*	  process_function: the function used to process the STREAMS of EXEC function.
+*	}
+**/
+function ssh_connect_execute(ssh_config, command_tring, processor) {
+  const debug_logger = require('debug')('command_runner.ssh_connect_execute');
+  
+	let conn = new ssh2();
+
+	conn.on(
+	    'connect',
+	    function () {
+	        debug_logger("DBEUG", "- connected" );
+	    }
+	);
+	 
+	conn.on(
+	    'ready',
+	    function() {
+	    	processor.connection = conn;
+			conn.exec(command_tring, processor.process_function);
+		}
+	);
+	 
+	conn.on(
+	    'error',
+	    function (err) {
+	        debug_logger("DBEUG", "- connection error: %s", err );
+	        conn.end();
+	    }
+	);
+	 
+	conn.on(
+	    'end',
+	    function () {
+	        debug_logger("DBEUG", "- connection ended");
+	    }
+	);
+	 
+	conn.connect(ssh_config);
+}
+
+function ssh_run_command(ssh_config, command, callback) {
+  const debug_logger = require('debug')('command_runner.ssh_run_command');
+  
+	let processor = {};
+	let exeData = {
+	  stdout: '',
+	  stderr: '',
+	  exit_code: -999999,
+	  exit_signal: -999999
+	};
+
+	processor.process_function = function(err, stream)  {
+		if(err) {
+			callback(err); //Forward the ERROR
+			return;
+		}
+
+    stream.on('data', function(data) {
+      debug_logger('STDOUT: ' + data);
+      exeData.stdout += data;
+    });
+    
+    stream.stderr.on('data', function(data) {
+      debug_logger('STDERR: ' + data);
+      exeData.stderr += data;
+    });
+
+		stream.on('close', function(code, signal) {
+			debug_logger('Exited with code ' + code + ' | signal: ' + signal);
+			processor.connection.end();
+			callback(null, exeData);
+		});
+	};
+
+	ssh_connect_execute(
+		ssh_config, 
+		command, 
+		processor
+	);
+}
+
+
 /**
  * Function "run"
  * This is MAIN function of ALL OK_Deployer Module
  * Params:
- *   @paramsObj: Object that contains all param sent to finction
- *      {param1: value1, param2: value2, ...}
+ *   @paramsObj: Object that contains all param sent to function
+ *      {   host: "192.168.8.110", 
+ *          port: 22,
+ *          username: "user",
+ *					command: "command to run",
+ *      }
+ *   @callback: callback function for async call
+ * 
+ * Callback error code:
+ *   Forward the error from SSH2 module
+ * 
+ * Callback Output data:
+ *	{
+ *    stdout: "stdout at remote",
+ *		stderr: "stderr ot remote"
+ *  	exit_code: value_of_exit_code
+ *  	exit_signal: value_of_exit_code
+ *	}
  */
 function run(paramsObj, callback)
 {
@@ -13,7 +123,18 @@ function run(paramsObj, callback)
   debug_logger('@paramsObj:');
   debug_logger(paramsObj);
   
-  callback(null, true);
+  let passphrase = process.env.PASSPHASE;
+  let privateKey = process.env.PRIVATE_KEY;
+
+  let ssh_config = {
+        "host": paramsObj.host,
+        "port": paramsObj.port,
+        "username": paramsObj.username,
+        "privateKey": require('fs').readFileSync(privateKey),
+        "passphrase": passphrase
+    };
+
+  ssh_run_command(ssh_config, paramsObj.command, callback);
 
   return;
 }
--- OK_Deployer/ok_modules/file_sender.js
+++ OK_Deployer/ok_modules/file_sender.js
@@ -1,9 +1,121 @@
+const fs = require('fs');
+const ssh2 = require('ssh2');
+
+/**
+ * Send a file to server, using SSH
+ * Params:
+ *   @ssh_config: config_object that contain information for SSH connect
+ *   @local_file: local file that need to send to server
+ *   @remote_file: file path on remote server
+ *   @callback: callback function for async call
+ * 
+ * Callback error code:
+ *   - 1: SFPT Starting error
+ *   - 2: SFPT Starting error
+ * 
+ * Callback Output data:
+ *	{
+ *  	local_file: "path_at_local",
+ *		remote_file:  "path_at_remote"
+ *	}
+ */
+function ssh_send_file(ssh_config, local_file, remote_file, callback) {
+  const debug_logger = require('debug')('file_sender.ssh_send_file');
+
+	let conn = new ssh2();
+
+	conn.on(
+	    'connect',
+	    function () {
+	        debug_logger( "- SSH connected" );
+	    }
+	);
+	 
+	conn.on(
+	    'ready',
+	    function () {
+	        debug_logger( "- SSH connection is ready" );
+	 
+	        conn.sftp(
+	            function (err, sftp) {
+	                if ( err ) {
+	                    debug_logger( "Error: problem starting SFTP: %s", err );
+	                    conn.end();
+	                    callback(2); //ERROR
+	                }
+	 
+	                debug_logger( "- SFTP started" );
+	 
+	                // upload file
+	                var readStream = fs.createReadStream(local_file);
+	                var writeStream = sftp.createWriteStream(remote_file);
+	 
+	                // what to do when transfer finishes
+	                writeStream.on(
+	                    'close',
+	                    function () {
+	                        console.log( "- file transferred" );
+	                        sftp.end();
+	                        conn.end();
+	                        //process.exit( 0 );
+
+	                        callback(null, {
+	                        	local_file: local_file,
+	                        	remote_file: remote_file
+	                        }); //SUCCESS
+	                    }
+	                );
+	 
+	                // initiate transfer of file
+	                readStream.pipe( writeStream );
+	            }
+	        );
+	    }
+
+	);
+	 
+	conn.on(
+	    'error',
+	    function (err) {
+	        debug_logger( "- SSH connection error: %s", err );
+	        conn.end();
+	        callback(1); //ERROR
+	    }
+	);
+	 
+	conn.on(
+	    'end',
+	    function () {
+	        debug_logger("- SSH connection ended");
+	    }
+	);
+	 
+	conn.connect(ssh_config);
+}
+
+
 /**
  * Function "run"
  * This is MAIN function of ALL OK_Deployer Module
  * Params:
- *   @paramsObj: Object that contains all param sent to finction
- *      {param1: value1, param2: value2, ...}
+ *   @paramsObj: Object that contains all param sent to function
+ *      {   host: "192.168.8.110", 
+ *          port: 22,
+ *          username: "root",
+ *					local_file: "file to send",
+ *					remote_file: "file at remote"
+ *      }
+ *   @callback: callback function for async call
+ * 
+ * Callback error code:
+ *   - 1: SFPT Starting error
+ *   - 2: SFPT Starting error
+ * 
+ * Callback Output data:
+ *	{
+ *  	local_file: "path_at_local",
+ *		remote_file:  "path_at_remote"
+ *	}
  */
 function run(paramsObj, callback)
 {
@@ -13,7 +125,18 @@ function run(paramsObj, callback)
   debug_logger('@paramsObj:');
   debug_logger(paramsObj);
   
-  callback(null, true);
+  let passphrase = process.env.PASSPHASE;
+  let privateKey = process.env.PRIVATE_KEY;
+
+  let ssh_config = {
+        "host": paramsObj.host,
+        "port": paramsObj.port,
+        "username": paramsObj.username,
+        "privateKey": require('fs').readFileSync(privateKey),
+        "passphrase": passphrase
+    };
+
+  ssh_send_file(ssh_config, paramsObj.local_file, paramsObj.remote_file, callback);
 
   return;
 }
